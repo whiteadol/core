03c933fe3476e5b430eae174e5383d9de0ec64d4
Jungtae <jeongtae.cho@omnious.com>
2022-02-23 20:37:03 +0900

    dddd

--- src/test/java/hello/core/RestAPITest.java
+++ src/test/java/hello/core/RestAPITest.java
@@ -40,7 +40,7 @@ public class RestAPITest {
             String output;
             System.out.println("Output from Sdddddderver .... \n");
             while ((output = br.readLine()) != null) {
-                System.out.println(output);
+                System.out.println(output + "tttttt");
             }
 
             conn.disconnect();
